eff1cd202eecd5916afb649783c0f4106b1bb427
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-12 13:11:23 -0600

    hhbhj

--- main.js
+++ main.js
@@ -2,47 +2,42 @@
 document.getElementById("btn").addEventListner("click", btnCLicked);
 
 function btnCLicked() {
+  let xA = +document.getElementById("xA").value;
+  let yA = +document.getElementById("yA").value;
+  let xB = +document.getElementById("xB").value;
+  let yB = +document.getElementById("yB").value;
+  let xC = +document.getElementById("xC").value;
+  let yC = +document.getElementById("yC").value;
 
-    let xA = +document.getElementById("xA").value;
-    let yA = +document.getElementById("yA").value;
-    let xB = +document.getElementById("xB").value;
-    let yB = +document.getElementById("yB").value;
-    let xC = +document.getElementById("xC").value;
-    let yC = +document.getElementById("yC").value;
+  let AB = dist(xA, yA, xB, yB);
+  let AC = dist(xA, yA, xC, yC);
+  let BC = dist(xB, yB, xC, yC);
+  let perimeter = peri(distanceA, distanceB, distanceC);
 
-   let AB = dist(xA, yA, xB, yB);
-    let AC = dist(xA, yA, xC, yC);
-    let BC = dist(xB, yB, xC, yC);
-    let perimeter = peri(distanceA, distanceB, distanceC);
+  document.getElementById("ABoutput").innnerHTML = AB;
+  doccument.getElementById("ACoutput").innnerHTML = AC;
+  document.getElementById("BCoutput").innnerHTML = BC;
+  document.getElementById("Perimeteroutput").innnerHTML = perimeter;
 
+  function dist(x1, y1, x2, y2) {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+    let distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  }
 
-    document.getElementById("ABoutput").innnerHTML = AB;
-    doccument.getElementById("ACoutput").innnerHTML = AC;
-    document.getElementById("BCoutput").innnerHTML = BC;
-    document.getElementById("Perimeteroutput").innnerHTML = perimeter;
-    
-
-    function dist(x1, y1, x2, y2) {
-        let xDistance = x2 - x1;
-        let yDistance = y2 - y1;
-        let distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-        return distance;
-    }
-    
-    function peri(distanceA, distanceB, distanceC) {
-        let perimeter = distanceA + distanceB + distanceC;
-        return perimeter;
-    }
+  function peri(distanceA, distanceB, distanceC) {
+    let perimeter = distanceA + distanceB + distanceC;
+  }
 }
 
 //function dist(x1, y1, x2, y2) {
-    //let xDistance = x2 - x1;
-    //let yDistance = y2 - y1;
-    //let distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-    //return distance;
+//let xDistance = x2 - x1;
+//let yDistance = y2 - y1;
+//let distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+//return distance;
 //}
 
 //function peri(distanceA, distanceB, distanceC) {
-    //let perimeter = distanceA + distanceB + distanceC;
-    //return perimeter;
+//let perimeter = distanceA + distanceB + distanceC;
+//return perimeter;
 //}
